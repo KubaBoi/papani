e826cb20f7d0e704aa35e97ef1b2dd46ed8b32ce
KubaBoi <jakubanderle@outlook.cz>
2021-09-15 17:53:47 +0200

    debug

--- main.py
+++ main.py
@@ -10,22 +10,3 @@ updateTime = 14400
 update = Updater(updateTime)
 update.updateAll()
 update.createHtmls()
-
-
-"""
-hostName = "localhost"
-hostPort = 80
-
-myServer = HTTPServer((hostName, hostPort), Server)
-print(time.asctime(), f"Server Starts - {hostName}:{hostPort}")
-
-try:
-    updateThread = threading.Thread(target=update.runForever)
-    updateThread.start()
-    myServer.serve_forever()
-except KeyboardInterrupt:
-    pass
-
-myServer.server_close()
-print(time.asctime(), f"Server Stops - {hostName}:{hostPort}")
-"""
